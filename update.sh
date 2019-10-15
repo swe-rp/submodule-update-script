f510e005c58d8677c04da0a8ab146ebfabd1dd12
lcfyi <leslie.cheng5@gmail.com>
2019-10-15 13:30:36 -0700

    Removed unnecessary push.

--- update.sh
+++ update.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 if [ ! -d $1 ]; then
     # Enter this block if the directory doesn't exist
     git clone --recurse-submodules $2 $1
@@ -12,7 +10,6 @@ cd $1
 # Update all the submodules within and push it
 git pull
 git submodule update --recursive --remote
-git push
 git add --all
 git commit -m "Updating submodule. This is done automatically."
 git push
